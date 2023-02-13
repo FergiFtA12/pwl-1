f2359f59c91ad0b4da20deadec9bc137987c9c21
Fergie Fatah Ardiansyah <fatahardiansyah12@gmail.com>
2023-02-13 11:31:51 +0700

    Tes a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Fergie Fatah Ardiansyah</title>
+        <title>Laravel Fergie Fatah Ardiansyah1</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
